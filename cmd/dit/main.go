22fad0eaccc8d799737dd12f0a2b53263503e2d2
Lars Lehtonen <lars.lehtonen@gmail.com>
2022-03-30 17:31:19 -0700

    cmd/dit: add -p precision flag

--- cmd/dit/main.go
+++ cmd/dit/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
 	"github.com/alrs/dit"
 )
 
+var precise bool
+
+func init() {
+	flag.BoolVar(&precise, "p", false, "show desek")
+	flag.Parse()
+}
+
 func main() {
 	now := time.Now()
 	d := dit.TimeToDIT(now)
-	fmt.Println(d)
+	if precise {
+		fmt.Println(d)
+	} else {
+		fmt.Printf("%d:%d\n", d.Dec(), d.Decim())
+	}
 }
